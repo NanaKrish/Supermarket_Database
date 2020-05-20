da48dfc4ed26a02ea0e3bb2307615bab39a23f2c
Narayanan Krishna <Narayanan Krishna@Cygnus-XS>
2020-05-20 18:43:39 +0530

    Final draft for Supermarket Database

---
+++ src/dbms_draft/DBMS_Draft.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms_draft;
+
+/**
+ *
+ * @author Narayanan Krishna
+ */
+public class DBMS_Draft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Opening_frame().setVisible(true); // TODO code application logic here
+    }
+    
+}
