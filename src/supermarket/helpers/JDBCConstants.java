1bda1dd07b82654b1cebd62a00b8c9c6317b8c7a
Sivagiri Visakan <siva.g.visakan@gmail.com>
2020-11-20 23:33:47 +0530

    Add helper classes

---
+++ src/supermarket/helpers/JDBCConstants.java
@@ -0,0 +1,11 @@
+package supermarket.helpers;
+
+public class JDBCConstants
+{
+
+    public static final String DRIVER_NAME = "java.sql.Driver";
+    public static final String URL         = "jdbc:mysql://localhost/";
+    public static final String USERNAME    = "root";
+    public static final String PASSWORD    = "";
+
+}
